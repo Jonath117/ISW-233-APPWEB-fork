fe8dd6acdf00e448760362b47994ecf3982cbc5d
Jonath117 <rochacontrerasjonathan40@gmail.com>
2025-05-15 16:59:21 -0400

    block card

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ function createCardComponent(title, body, image ){
 
 // 3. Cree un componente
 //createCardComponent('Hola', 'Hola soy un body');
-const card = createCardComponent('Hola', 'Hola soy un body', 'images/Maxsucho pensativo.jpg');
+const card = createCardComponent('Hola', 'Hola soy un body, ddnkjdsnfjdsnfsin sdjfndsiuncdsiucnds sdnfoidsncodsicn unoisadmosaidm oiad fdsf dsjfndsun odsjfniudsfndsuifbdsiuf sodjfnodsiufndsoifnddf kjf sdjfnosiudfnodsufnusn osdifndsufbsiubs asdsfdsf ssdfdsf dsfdsfdsf ssdfsd', 'images/Maxsucho pensativo.jpg');
+const card2 = createCardComponent('Hola', 'Hola soy un body extremadamente largo, chau', 'images/Maxsucho pensativo.jpg');
 
 // 4. Agrege este nuevo componente al container
 container.appendChild(card);
+container.appendChild(card2);
