16ab243b99ea38df7d10ebc83154e4f2f24c7563
Jonath117 <rochacontrerasjonathan40@gmail.com>
2025-05-15 14:07:53 -0400

    añadi el card y los diseños que faltaban
    Terminé de completar el navbar y el footer, además añadí la lógica para la creación del card

--- index.js
+++ index.js
@@ -8,19 +8,38 @@ const container = document.getElementById('container');
 /**
  * @param {string} title
  * @param {string} body
- *
+ * @param {string} image
  * @return {HTMLElement}
  */
 
-function createCardComponent(title, body ){
-  const template = document.getElementById('card_template');
+
+function createCardComponent(title, body, image ){
+  const template = document.getElementById('card-template');
   const element = template.content.cloneNode(true).firstElementChild;
-  const titleElement = element.getElementByTagname('h3');
+
+  const titleElement = element.querySelector('.card__title');
+  const bodyElement = element.querySelector('.card__body__content');
+  const imageContainer = element.querySelector('.card__body__image');
+
+  titleElement.textContent = title;
+  bodyElement.textContent = body;
+
+  const img = document.createElement('img');
+  img.src = 'images/Maxsucho pensativo.jpg';
+  img.alt = title;
+  img.classList.add('card__body__image');
+
+  imageContainer.appendChild(img);
+
   console.log(element);
+
   return element
 }
 
+
 // 3. Cree un componente
-createCardComponent('Hola', 'Hola soy un body');
+//createCardComponent('Hola', 'Hola soy un body');
+const card = createCardComponent('Hola', 'Hola soy un body', 'images/Maxsucho pensativo.jpg');
 
 // 4. Agrege este nuevo componente al container
+container.appendChild(card);
