dc4ed0b47951e6adee5fc8c3ef6eaa0367ee676a
Jonath117 <rochacontrerasjonathan40@gmail.com>
2025-05-15 21:21:17 -0400

    Acomodar footer
    Intenté y no pude hacer que el footer cubra toda la parte de abajo

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ function createCardComponent(title, body, image ){
 
 // 3. Cree un componente
 //createCardComponent('Hola', 'Hola soy un body');
-const card = createCardComponent('Hola', 'Hola soy un body, ddnkjdsnfjdsnfsin sdjfndsiuncdsiucnds sdnfoidsncodsicn unoisadmosaidm oiad fdsf dsjfndsun odsjfniudsfndsuifbdsiuf sodjfnodsiufndsoifnddf kjf sdjfnosiudfnodsufnusn osdifndsufbsiubs asdsfdsf ssdfdsf dsfdsfdsf ssdfsd', 'images/Maxsucho pensativo.jpg');
-const card2 = createCardComponent('Hola', 'Hola soy un body extremadamente largo, chau', 'images/Maxsucho pensativo.jpg');
+const card = createCardComponent('Fundamentals 1', 'Hola soy un body, ddnkjdsnfjdsnfsin sdjfndsiuncdsiucnds sdnfoidsncodsicn unoisadmosaidm oiad fdsf dsjfndsun odsjfniudsfndsuifbdsiuf sodjfnodsiufndsoifnddf kjf sdjfnosiudfnodsufnusn osdifndsufbsiubs asdsfdsf ssdfdsf dsfdsfdsf ssdfsd', 'images/Maxsucho pensativo.jpg');
+const card2 = createCardComponent('Fundamentals 2', 'Hola soy un body extremadamente largo, chau', 'images/Maxsucho pensativo.jpg');
 
 // 4. Agrege este nuevo componente al container
 container.appendChild(card);
